94889ebecc94bee4d4ed7b639069cc049b028f93
RitaDyvnych <m.dyvnych@gmail.com>
2021-10-20 17:47:57 +0300

    компоненты форм переписаны с классов на хуки

--- src/components/ContactList.js
+++ src/components/ContactList.js
@@ -1,19 +1,18 @@
-import React, { Component } from "react";
+import React from "react";
 import PropTypes from "prop-types";
 import style from "./contacts.module.css";
 
-class ContactList extends Component {
-  render() {
-    return (
+export default function ContactList({ contacts, handleDelete }) {
+  return (
       <ul className={style.list}>
-        {this.props.contacts.map((el) => (
+        {contacts.map((el) => (
           <li className={style.listItem} key={el.id}>
             {el.name}: {el.number}
             <button
               type="button"
               className={style.btnDelete}
               id={el.id}
-              onClick={this.props.handleDelete}
+              onClick={handleDelete}
             >
               Delete
             </button>
@@ -21,10 +20,7 @@ class ContactList extends Component {
         ))}
       </ul>
     );
-  }
 }
-
-export default ContactList;
 
 ContactList.propTypes = {
   contacts: PropTypes.array.isRequired,
